77ae60af2d74c686ad5f82a75c84252889bd1e86
Munira Akter <munira660602@gmail.com>
2021-10-13 17:46:17 +0600

    Laravel 8 Controller namespace register in RouteServiceProvider

---
+++ app/readme.md
@@ -0,0 +1,4 @@
+## Laravel 8 Controller namespace register in RouteServiceProvider
+
+First of all we have to set our controller namespace in RouteServiceprovide
+As a global call then we can easilly write our controller name in route file without use namespace of controller
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+/*
+|--------------------------------------------------------------------------
+| Laravel 8 Controller namespace register in RouteServiceProvider
+|--------------------------------------------------------------------------
+|
+|First of all we have to set our controller namespace in RouteServiceprovide
+| As a global call then we can easilly witre our controller name in route file
+|without use namespace of controller
+|
+*/
+
+
+Route::get('/', 'PracticeController@index');
